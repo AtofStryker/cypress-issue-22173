f049d3e50545a5ba153f8410582a4083b889186e
Bill Glesias <bglesias@gmail.com>
2022-06-17 18:28:36 -0400

    add reproducible example, credit to @jordanhennell

--- cypress/integration/screenshot.spec.js
+++ cypress/integration/screenshot.spec.js
@@ -1,15 +1,9 @@
 describe("drive screenshot", () => {
     it("screenshots", () => {
-      cy.on("uncaught:exception", () => false);
-      // public drive folder that only has view permissions
-      cy.visit(
-        "https://drive.google.com/drive/folders/1xnJosjV3GcnMGpjKZz6eiMHM_rWIvLOF"
-      );
-      cy.get('[data-tooltip="List view"]').click();
-      cy.wait(300);
-      cy.get('[role="gridcell"]').first().get('[data-column-field="5"]');
-      cy.screenshot({
-        blackout: [`[data-column-field="5"]`],
+      cy.visit("https://www.wikipedia.org/")
+      .get('.central-featured') // centered (margin: 0 auto) globe with languages around it
+      .screenshot({
+          blackout: ['[lang=en]']
       });
     });
   });
